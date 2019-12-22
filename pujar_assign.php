d4c090ca4b1b6aa7f42d13306460072bc25a8209
mdlunag <58974716+mdlunag@users.noreply.github.com>
2019-12-22 18:28:50 +0100

    missatge error files

--- pujar_assign.php
+++ pujar_assign.php
@@ -7,6 +7,16 @@ include_once 'app/Conexio.inc.php';
 include_once 'app/RepositoriAssignatures.inc.php';
 include_once 'app/assignatura.inc.php';
 
+$errors = array(1, 2, 3, 4, 5, 6, 7, 8);
+$llista_errors = array(
+    1 => "El tamany de l'arxiu supera el màxim permès",
+    2 => "El tamany de l'arxiu supera el màxim permès",
+    3 => "L'arxiu s'ha pujat parcialment",
+    4 => 'Has de pujar un fitxer!',
+    6 => 'Falta una carpeta temporal',
+    7 => "Error en escriure l'arxiu al disc",
+    8 => "Una extensió de PHP ha aturat la pujada de l'arxiu."
+);
 
 if (isset($_POST['uploadBtn2']) && $_POST['uploadBtn2'] == 'Upload') {
     if (isset($_FILES['arxiu_assign']) && $_FILES['arxiu_assign']['error'] === UPLOAD_ERR_OK) {
@@ -27,6 +37,7 @@ if (isset($_POST['uploadBtn2']) && $_POST['uploadBtn2'] == 'Upload') {
             $lineas = file($archivotmp);
 
             $i = 0;
+             $afegides=0;
             Conexio :: obrir_conexio();
 
 
@@ -55,24 +66,23 @@ if (isset($_POST['uploadBtn2']) && $_POST['uploadBtn2'] == 'Upload') {
                     $assign_afegida = RepositoriAssignatures :: afegir_assign(Conexio :: obtenir_conexio(), $assign);
 
                     if ($assign_afegida) {
-                        $message4 = 'Afegides ' . $i . ' assign';
+                       $afegides++;
                     }
                 }
                 $i++;
             }
+            $message4 = 'Afegides ' . $afegides . ' assignatures';
             Conexio :: tancar_conexio();
-
-
-            
-        } else {
-            $message3 = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
-        }
+            } else {
+            $message3 = "Error en el format de l'arxiu. Extensions permeses: " . implode(',', $allowedfileExtensions);
+        }           
+    } elseif (in_array($_FILES['arxiu_assign']['error'], $errors, true)) {
+        $message3 = $llista_errors[$_FILES['arxiu_assign']['error']];
     } else {
-        $message3 = 'There is some error in the file upload. Please check the following error.<br>';
-        $message3 .= 'Error:' . $_FILES['arxiu_assign']['error'];
+        
+        $message3 = "Hi ha algun error desconegut amb la pujada de l'arxiu.";
     }
 }
-
 
 
 $_SESSION['message3'] = $message3;
--- pujar_profes.php
+++ pujar_profes.php
@@ -13,8 +13,16 @@ Conexio::obrir_conexio();
 $total_usuaris = RepositoriProfessors :: obtenir_num_professors(Conexio::obtenir_conexio());
 $conexio = Conexio::obtenir_conexio();
 Conexio :: tancar_conexio();
-
-
+$errors = array(1, 2, 3, 4, 5, 6, 7, 8);
+$llista_errors = array(
+    1 => "El tamany de l'arxiu supera el màxim permès",
+    2 => "El tamany de l'arxiu supera el màxim permès",
+    3 => "L'arxiu s'ha pujat parcialment",
+    4 => 'Has de pujar un fitxer!',
+    6 => 'Falta una carpeta temporal',
+    7 => "Error en escriure l'arxiu al disc",
+    8 => "Una extensió de PHP ha aturat la pujada de l'arxiu."
+);
 if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload') {
     if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
         $fileName = $_FILES['file']['name'];
@@ -34,56 +42,55 @@ if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload') {
             $lineas = file($archivotmp);
 
             $i = 0;
-            $afegits=0;
+            $afegits = 0;
             Conexio :: obrir_conexio();
             $conexio = Conexio::obtenir_conexio();
 
             foreach ($lineas as $linea_num => $linea) {
 
                 if ($i != 0) {
-					if (strpos($linea, ';') !== false){
-     $datos = explode(";", $linea);
-                 
-}else{
-      
-       $datos = explode(",", $linea);
+                    if (strpos($linea, ';') !== false) {
+                        $datos = explode(";", $linea);
+                    } else {
 
-}
-                 
+                        $datos = explode(",", $linea);
+                    }
+
 
                     $nom = $datos[0];
                     $cognoms = $datos[1];
                     $email = strval($datos[2]);
                     $punts = $datos[3];
                     $contra = rtrim($datos[4]);
-                    $estat=intval(0);
-                    
-                    if ($total_usuaris==0 && $i==1){
-                        $estat=intval(1);
+                    $estat = intval(0);
+
+                    if ($total_usuaris == 0 && $i == 1) {
+                        $estat = intval(1);
                     }
-                 
-                    $usuari = new Professor('', $nom, $cognoms, $email, $punts,'','',$contra, '', $estat,'');
+
+                    $usuari = new Professor('', $nom, $cognoms, $email, $punts, '', '', $contra, '', $estat, '');
                     //$usuari = new Professor('', $nom, $cognoms, $email, $punts,'','',password_hash($contra,PASSWORD_DEFAULT), '', '','');
 
-                    $usuari_afegit = RepositoriProfessors :: afegir_professor(Conexio :: obtenir_conexio(), $usuari,$total_usuaris);
-                   
+                    $usuari_afegit = RepositoriProfessors :: afegir_professor(Conexio :: obtenir_conexio(), $usuari, $total_usuaris);
+
                     if ($usuari_afegit) {
-                       $afegits++;
+                        $afegits++;
                     }
                 }
                 $i++;
             }
-             $message2 = 'Afegits ' . $afegits . ' professors';
+            $message2 = 'Afegits ' . $afegits . ' professors';
             Conexio :: tancar_conexio();
 
 
             $message = '';
         } else {
-            $message = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
+            $message = "Error en el format de l'arxiu. Extensions permeses: " . implode(',', $allowedfileExtensions);
         }
+    } elseif (in_array($_FILES['file']['error'], $errors, true)) {
+        $message = $llista_errors[$_FILES['file']['error']];
     } else {
-        $message = 'There is some error in the file upload. Please check the following error.<br>';
-        $message .= 'Error:' . $_FILES['file']['error'];
+        $message = "Hi ha algun error desconegut amb la pujada de l'arxiu.";
     }
 }
 
